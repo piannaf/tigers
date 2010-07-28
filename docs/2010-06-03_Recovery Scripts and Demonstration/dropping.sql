84119266fdeebdfbd7ebdaf29d6ac2f86e58c551
jmancine <jmancine@c45332e1-4c21-8192-b9cb-2cdf34cb180e>
2010-07-28 02:16:35 +0000

    Semester 1 Deliverables have been added to Docs folder

---
+++ docs/2010-06-03_Recovery Scripts and Demonstration/dropping.sql
@@ -0,0 +1,37 @@
+drop TABLE Contractor;
+drop TABLE Laboratory;
+drop view ViewLaboratory;
+drop view ViewContractor;
+
+drop TABLE EnvironmentalOfficer;
+drop TABLE ScreeningProgram;
+drop view ViewEnvironmentalOfficer;
+drop view ViewScreeningProgram;
+
+drop TABLE Sample;
+drop TABLE Sampler;
+drop view ViewSample;
+drop view ViewSampler;
+
+drop TABLE ParameterNames;
+drop TABLE FrequencyItem;
+drop view ViewParameterNames;
+drop view ViewFrequencyItem;
+
+drop TABLE ScreeningItem;
+drop TABLE ScreeningProgramSamplers;
+drop view ViewScreeningItem;
+drop view ViewScreeningProgramSamplers;
+
+drop TABLE ParameterThresholds;
+drop TABLE SamplerMedia;
+drop view ViewParameterThresholds;
+drop view ViewSamplerMedia;
+
+drop TABLE ScreeningFrequency;
+drop TABLE TigersUser;
+drop view ViewScreeningFrequency;
+drop view ViewTigersUser;
+
+drop TABLE Waterbody;
+drop view ViewWaterbody;
