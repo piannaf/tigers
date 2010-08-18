c233620bbf5b178b2b30bd4f73e9b86fc7c36d80
jane.harrison@uqconnect.edu.au <jane.harrison@uqconnect.edu.au@c45332e1-4c21-8192-b9cb-2cdf34cb180e>
2010-08-18 15:16:02 +0000

    Upload Daos

--- tigers/src/main/java/com/tiffany/dao/SamplerDao.java
+++ tigers/src/main/java/com/tiffany/dao/SamplerDao.java
@@ -5,9 +5,10 @@ import java.util.List;
 import com.tiffany.model.Sample;
 import com.tiffany.model.Sampler;
 
-public interface SamplerDao extends GenericDao<Sampler, String> {
-   
+public interface SamplerDao extends GenericDao<Sampler, Long> {
+
     public List<Sampler> findByWaterBody(String waterBody);
+    public Sampler findByTag(String tag);
     public Sampler getByTag(String tag);
     public List<Sampler> findByLaboratory(String laboratory);
     public List<Sampler> findByTag(String tag);
