8309feebe5b20cb912cb476ac6eb4ea9631351cf
jane.harrison@uqconnect.edu.au <jane.harrison@uqconnect.edu.au@c45332e1-4c21-8192-b9cb-2cdf34cb180e>
2010-08-18 15:19:27 +0000

    Uploading pojos

--- tigers/src/main/java/com/tiffany/model/Waterbody.java
+++ tigers/src/main/java/com/tiffany/model/Waterbody.java
@@ -2,6 +2,8 @@ package com.tiffany.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,11 +15,20 @@ import javax.persistence.Table;
 @Entity
 @Table(name="Waterbody")
 public class Waterbody extends BaseObject {
-
+	
+	private Long id;
 	private String name;
 	private char type;
 	
-	@Id @Column(nullable=false)
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	@Column(nullable=false)
 	public String getName() {
 		return name;
 	}
@@ -31,18 +42,15 @@ public class Waterbody extends BaseObject {
 	public void setType(char type) {
 		this.type = type;
 	}
-	@Override
-	public String toString() {
-		return "Waterbody [name=" + name + ", type=" + type + "]";
-	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + type;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -52,14 +60,19 @@ public class Waterbody extends BaseObject {
 		if (getClass() != obj.getClass())
 			return false;
 		Waterbody other = (Waterbody) obj;
-		if (name == null) {
-			if (other.name != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (type != other.type)
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		return "Waterbody [id=" + id + ", name=" + name + ", type=" + type
+				+ "]";
+	}
+	
 
 }
